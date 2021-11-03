5b33f6656de970225ae3eb26218d34e09409e398
LeoMosiah <leonardomosiah@gmail.com>
2021-11-03 11:42:46 +0100

    add visual cue

--- RazorMegaDesk/Pages/DeskQuotes/Create.cshtml.cs
+++ RazorMegaDesk/Pages/DeskQuotes/Create.cshtml.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using RazorMegaDesk.Data;
 using RazorMegaDesk.Models;
 
 namespace RazorMegaDesk.Pages.DeskQuotes
@@ -43,7 +38,5 @@ namespace RazorMegaDesk.Pages.DeskQuotes
 
             return RedirectToPage("./Index");
         }
-
-
     }
 }
