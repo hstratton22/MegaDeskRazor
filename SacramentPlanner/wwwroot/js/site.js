e54f07848bd47bb510158939ee7a86a8bdf4e5d1
Heather Stratton <hastratton@gmail.com>
2021-12-14 06:54:35 -0500

    mod print button, etc

--- SacramentPlanner/wwwroot/js/site.js
+++ SacramentPlanner/wwwroot/js/site.js
@@ -1,7 +1,7 @@
 ﻿
     function PrintDiv() {
                 var divToPrint = document.getElementById('printArea');
-    var popupWin = window.open('', '_blank', 'width=500,height=700,location=no,left=200px');
+    var popupWin = window.open('', '_blank', 'width=600,height=800,location=no,left=200px');
     popupWin.document.open();
     popupWin.document.write('<html><body onload="window.print()">' + divToPrint.innerHTML + '</html>');
         popupWin.document.close();
