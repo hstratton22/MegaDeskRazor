24930166b9833bcf2c34169dbc39169351533767
Heather Stratton <hastratton@gmail.com>
2021-12-14 06:23:58 -0500

    added print button and functionality

--- SacramentPlanner/wwwroot/js/site.js
+++ SacramentPlanner/wwwroot/js/site.js
@@ -1,4 +1,9 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿
+    function PrintDiv() {
+                var divToPrint = document.getElementById('printArea');
+    var popupWin = window.open('', '_blank', 'width=500,height=700,location=no,left=200px');
+    popupWin.document.open();
+    popupWin.document.write('<html><body onload="window.print()">' + divToPrint.innerHTML + '</html>');
+        popupWin.document.close();
+            }
+      
