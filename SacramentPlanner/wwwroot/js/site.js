5eaa06ffbb798b4c00f6648cbe01e40b1c967500
LeoMosiah <leonardomosiah@gmail.com>
2021-12-14 14:32:15 +0100

    remove elements from print

--- SacramentPlanner/wwwroot/js/site.js
+++ SacramentPlanner/wwwroot/js/site.js
@@ -1,9 +1,5 @@
 ﻿
-    function PrintDiv() {
-                var divToPrint = document.getElementById('printArea');
-    var popupWin = window.open('', '_blank', 'width=600,height=800,location=no,left=200px');
-    popupWin.document.open();
-    popupWin.document.write('<html><body onload="window.print()">' + divToPrint.innerHTML + '</html>');
-        popupWin.document.close();
+function PrintDiv() {
+    window.print()
             }
       
