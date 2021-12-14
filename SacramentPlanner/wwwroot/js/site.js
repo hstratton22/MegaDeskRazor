820ff5a5071254bd39961e0e18afce0ff8b22c2b
LeoMosiah <leonardomosiah@gmail.com>
2021-12-14 16:06:45 +0100

    revert changes

--- SacramentPlanner/wwwroot/js/site.js
+++ SacramentPlanner/wwwroot/js/site.js
@@ -1,5 +1,9 @@
 ﻿
 function PrintDiv() {
-    window.print()
+    var divToPrint = document.getElementById('printArea');
+    var popupWin = window.open('', '_blank', 'width=600,height=800,location=no,left=200px');
+    popupWin.document.open();
+    popupWin.document.write('<html><body onload="window.print()">' + divToPrint.innerHTML + '</html>');
+    popupWin.document.close();
             }
       
